c80f16426de20d200b49cd7f3b319fd24a57e95a
TDGerve <thomasvangerve@gmail.com>
2023-01-09 15:40:13 +0100

    update

--- setup.py
+++ setup.py
@@ -2,14 +2,13 @@ import setuptools
 
 setuptools.setup(
     name= 'ramCOH',
-    version= '0.1',
+    version= '1.0',
     description= '...',
     author= 'Thomas van Gerve',
     
     packages= setuptools.find_packages(where=["ramCOH/"]
         ),
 
-    # package_dir= {'' : 'petroPy'},
     package_data= {'ramCOH': ['static/*']}, 
 
     install_requires= [
