883a32a9e9509ec5f324e7522547336fa2856eba
TDGerve <thomasvangerve@gmail.com>
2023-03-03 22:02:16 +0100

    update

--- ramCOH/raman/__init__.py
+++ ramCOH/raman/__init__.py
@@ -1,4 +1,4 @@
 from ramCOH.raman.baseclass import *
-from ramCOH.raman.CO2 import *
-from ramCOH.raman.Glass import *
+from ramCOH.raman.co2 import *
+from ramCOH.raman.glass import *
 from ramCOH.raman.neon import *
