fd1a00c09f98016142b6a23953bea6303e5ee4c5
TDGerve <thomasvangerve@gmail.com>
2023-03-03 22:50:45 +0100

    update

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 import setuptools
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setuptools.setup(
     name="ramCOH",
     version="1.0",
     description="Library for processing and peak fitting of Raman spectra, targeted at CO2 fluids and hydrous silicate glasses",
     author="Thomas van Gerve",
     url="https://github.com/TDGerve/ramCOH",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=setuptools.find_packages(where=["ramCOH/"]),
     package_data={"ramCOH": ["static/*"]},
     install_requires=[
@@ -14,6 +19,11 @@ setuptools.setup(
         "numpy",
         "scipy",
         "csaps",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
     python_requires=">=3.8",
 )
